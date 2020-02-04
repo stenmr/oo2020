70961fbd5fc798a5d60066fa8d6bdbeafc13057e
vechro <stenmr@gmail.com>
2020-02-04 12:00:34 +0200

    Changes + new function

--- w01_funktsioon/HelloWorld.java
+++ w01_funktsioon/HelloWorld.java
@@ -1,7 +1,7 @@
 package w01_funktsioon;
 
 public class HelloWorld {
-    public static void main(String[] art) {
+    public static void main(String[] args) {
         System.out.print("Hello World");
 
         byte myByte = Byte.MIN_VALUE;
