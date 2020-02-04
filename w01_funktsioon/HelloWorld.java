3e191a03188cf2a37a9631f8ac1aebe2ce9aef40
vechro <stenmr@gmail.com>
2020-02-04 11:16:31 +0200

    learning java

--- w01_funktsioon/HelloWorld.java
+++ w01_funktsioon/HelloWorld.java
@@ -1,6 +1,63 @@
-public class HelloWorld {
+package w01_funktsioon;
 
+public class HelloWorld {
     public static void main(String[] art) {
         System.out.print("Hello World");
+
+        byte myByte = Byte.MIN_VALUE;
+        // byte, short, int, long, float, double, char, boolean
+
+        boolean flag = true;
+
+        System.out.println(1 + 1);
+        int number = 5;
+        if (number < 6 && number > 0) {
+            System.out.println("Jah");
+        }
+
+        for (int i = 0; i < 10; i++) {
+
+        }
+
+        while (flag) {
+            number++;
+            if (number == 10) {
+                System.out.println("I'm out");
+                flag = false;
+            }
+        }
+
+        int[] numberArray = new int[] {1, 2, 3, 4, 5};
+
+        for(int element : numberArray) {
+            System.out.println(element);
+        }
+
+        for (int i = 0; i < numberArray.length; i++) {
+            System.out.println(numberArray[i]);
+        }
+
+        String myName = "Sten";
+
+        System.out.println(myName);
+
+        printSum(5);
+
+
+        String name = name();
+        System.out.println(name);
+    }
+
+    static String name() {
+        return "Sten";
+    }
+
+    
+    static void printSum(int number) {
+        int sum = 0;
+        for (; number < number + 20; number++) {
+            sum = sum + number;
+        }
+        System.out.println(sum);
     }
 }
